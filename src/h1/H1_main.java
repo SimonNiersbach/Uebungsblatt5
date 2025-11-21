9c74304854cb8ea7e3d504c8e43cea25e912ce31
Simon Niersbach <simon-niersbach@Mac.localdomain>
2025-11-21 20:22:21 +0100

    Initial Commit

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,4 +1,14 @@
 package h1;
 
 public class H1_main {
+    public static void main(String[] args) {
+        int [] myArray = {5,6,7,10};
+        int [] myArray2 = new int[myArray.length];
+
+        for(int i = 0; i < myArray.length; i++){
+           // System.out.println(myArray[(myArray.length - 1) - i]);
+            myArray2[i] = myArray[(myArray.length - 1) - i];
+        }
+        myArray = myArray2;
+    }
 }
--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,4 +1,17 @@
 package h2;
 
 public class H2_main {
+    public static void main(String[] args) {
+        int n = 299;
+        int digits;
+        int [] a = {0,0,0,0,0,0,0,0,0};
+
+        digits = (int) Math.log10(n) + 1;
+
+
+        for(int i = 0; i < digits; i++){
+            a[(a.length - 1) - i] = n % 10;
+            n = (int) n / 10;
+        }
+    }
 }
