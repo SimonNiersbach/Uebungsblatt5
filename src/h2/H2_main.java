4daa9a39b361492776a632336652218d16462fb1
Simon Niersbach <simon-niersbach@Mac.localdomain>
2025-11-21 20:35:11 +0100

    Fixed handling of the number 0 in h2/H2_main

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -6,7 +6,11 @@ public class H2_main {
         int digits;
         int [] a = {0,0,0,0,0,0,0,0,0};
 
-        digits = (int) Math.log10(n) + 1;
+        if (n == 0) {
+            digits = 1;
+        } else {
+            digits = (int) Math.log10(n) + 1;
+        }
 
 
         for(int i = 0; i < digits; i++){
